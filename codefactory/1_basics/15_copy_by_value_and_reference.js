86922a4f430e7bf5b271dd70e758ae8b4f96e486
강동욱 <ehddnr8563@gmail.com>
2024-01-30 22:03:24 +0900

    1/30

---
+++ codefactory/1_basics/15_copy_by_value_and_reference.js
@@ -0,0 +1,11 @@
+/**
+ * copy by value 값에 의한 전달
+ * copy by reference 참조에 의한 전달
+ *
+ * 1) 기본적으로 모든 primitive 값은 copy by value다
+ * 2) 객체는 copy by reference다
+ */
+let original = '안녕하세요';
+let clone = original;
+console.log(original);
+console.log(clone);
