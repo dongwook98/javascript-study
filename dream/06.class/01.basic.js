462d38041be4139245b088b1159b64dde578f283
강동욱 <ehddnr8563@gmail.com>
2024-02-11 23:24:34 +0900

    클래스 기본

--- dream/06.class/01.basic.js
+++ dream/06.class/01.basic.js
@@ -2,16 +2,19 @@
 // 1. 생성자 함수 (오래된 고전적인 방법)
 // 2. 클래스 ✨
 
-// 클래스 class
+// 클래스 class 만드는 방법
 class Fruit {
   name;
   emoji;
+
   // constructor(생성자): new 키워드로 객체를 생성할때 호출되는 함수
+  // 생성자로 객체를 만들때 필요한 데이터를 외부로 부터 전달받을 수 있음
   constructor(name, emoji) {
     this.name = name;
     this.emoji = emoji;
   }
 
+  // 클래스 메서드에는 function 키워드 적으면 안됌
   display = () => {
     console.log(`${this.name}: ${this.emoji}`);
   };
