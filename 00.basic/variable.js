453ee36aa7be4439965a7bcb25f7388b9f16833e
강동욱 <ehddnr8563@gmail.com>
2024-02-24 22:38:32 +0900

    file move

---
+++ 00.basic/variable.js
@@ -0,0 +1,22 @@
+// 1. Use strict
+// added in ES 5
+// use this for valina Javascript.
+'use strict';
+
+// 2. Variable
+// let (added in ES6)
+let a;
+a = 6;
+
+/**
+ * Web API
+ * 자바스크립트가 제공하는 것 X
+ * 브라우저가 제공하는 것
+ * Console API는 브라우저, 노드에서 둘 다 제공
+ */
+
+/**
+ * 자바스크립트 공식사이트는?
+ * ecma-international.org <- 너무 난해함
+ * developer.mozilla.org <- 추천
+ */
